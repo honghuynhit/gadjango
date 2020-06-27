3041b8ed73ae19099e2e7d35e5bf2c942b3f0e2c
huynh ga <honghuynhit@gmail.com>
2020-06-27 18:54:29 +0700

    update

--- gadjango/settings.py
+++ gadjango/settings.py
@@ -73,13 +73,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gadjango.wsgi.application'
 
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+# SECRET_KEY = config('SECRET_KEY')
+# DEBUG = config('DEBUG', default=False, cast=bool)
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=config('DATABASE_URL')
+#     )
+# }
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
